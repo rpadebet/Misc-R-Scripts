ad974c7f633ec81e2979fa88ed71a35697f1427a
Rohit Padebettu <rohit.pittu@gmail.com>
2016-11-13 01:08:15 -0500

    Postgres database connection

---
+++ Database connection.R
@@ -0,0 +1,17 @@
+library(RPostgreSQL)
+
+drv<- dbDriver("PostgreSQL")
+conn<-dbConnect(drv,dbname="spam",host="localhost",port=5432,user="rohit",password="123")
+
+data("iris")
+dbWriteTable(conn,'iris',iris, row.names=FALSE)
+dbListTables(conn)
+
+dtab = dbGetQuery(conn, "select * from iris")
+summary(dtab)
+
+library(quantmod)
+
+class(getSymbols('MSFT',src='yahoo'))
+
+str(MSFT$MSFT)
