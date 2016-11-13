79c62ec64e9d43893768591e430a4139748b37c4
Rohit Padebettu <rohit.pittu@gmail.com>
2016-11-13 11:55:39 -0500

    spark h2o R

--- sparklyr h20.R
+++ sparklyr h20.R
@@ -3,11 +3,22 @@ library(rsparkling)
 library(h2o)
 library(dplyr)
 
-# connect to spark
-sc <- spark_connect("local", version = "1.6.2")
+## connect to spark - check if needed - two instances installed
+# Sys.setenv(SPARK_HOME = "/usr/local/src/Spark")
+# Sys.setenv(SPARK_HOME = "/home/rohit/.cache/spark")
+
+# Sys.getenv("SPARK_HOME")
+
+# spark connection
+sc <- spark_connect(master = "local")
 
 # copy mtcars dataset into spark
 mtcars_tbl <- copy_to(sc, mtcars, "mtcars", overwrite = TRUE)
+
+#using sql with spark
+library(DBI)
+mtcars_preview <- dbGetQuery(sc, "SELECT * FROM mtcars LIMIT 10")
+mtcars_preview
 
 # transform our data set, and then partition into 'training', 'test'
 partitions <- mtcars_tbl %>%
@@ -16,7 +27,7 @@ partitions <- mtcars_tbl %>%
   sdf_partition(training = 0.5, test = 0.5, seed = 1099)
 
 training <- as_h2o_frame(partitions$training)
-test <- as_h2o_frame(partitions$test)
+test <- as_h2o_frame(sc,partitions$test)
 
 # fit a linear model to the training dataset
 fit <- h2o.glm(x = c("wt", "cyl"), 
